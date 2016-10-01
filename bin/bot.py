bbdf820b6b7540190ee101e010544f755848221b
Alexander Lukyanchenko <digwnews@gmail.com>
2016-10-01 12:05:54 +0000

    Add keep for lib

--- bin/bot.py
+++ bin/bot.py
@@ -9,9 +9,14 @@ bot = Bot(api_token=os.environ['API_KEY'])
 @bot.command(r'/start')
 def start(chat, match):
     markup = {
-        "keyboard": [["Make a bet"], ["Show rating"], ["F**k off"]],
+        "keyboard": [["Make a bet"], ["Show rating"], ["Your bets"]],
         "one_time_keyboard": True
     }
     return chat.send_text('Hi!', reply_markup=json.dumps(markup))
+
+
+@bot.command(r'/makebet')
+def make_bet(chat, match):
+    pass
 
 bot.run()
